f604ded19f0cd347b8dc17303b10427b089eb05c
Gray-maker <ivygray39@gmail.com>
2025-11-26 17:36:19 +0300

    Add files via upload

---
+++ Invoice1.java
@@ -0,0 +1,41 @@
+public class Invoice {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("|                              TAX INVOICE                                      |");
+        System.out.println("--------------------------------------------------------------------------------");
+
+        System.out.println("Bill To:                                Ship To:");
+        System.out.println("C026-01-0750/2024                       GATHONI IVY GRACE");
+        System.out.println("No 23/2, SBI Colony.                    No 23/2, SBI Colony.");
+        System.out.println("Ragavendra Nagar, Chennai-600124        Ragavendra Nagar, Chennai-600124");
+        System.out.println("GSTIN:33APFSD1ZV                        GSTIN:33APFSD1ZV");
+        System.out.println("Contact:+91-7869825463                  Contact: +91-7869825463");
+        System.out.println("Email: purchase-team@thendral.com        Email: purchase-team@thendral.com");
+
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("Payment Date : 7 days from date of delivery   Purchase Terms: 7 days from date of delivery");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("| S.No | Code | Product Name        | HSN Code | Qty |  Rate   | Tax | Amount   |");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("|  1   | 105  | Surf Excel 5 kg     | 34019011 | 20  | 600.00  | 5%  | 12600.00 |");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("|  2   | 109  | Rin 1 kg            | 34019011 | 25  |  85.00  | 5%  |  2125.00 |");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("|  3   | 115  | Hamam soap 150 g    | 34011100 | 25  |  60.00  | 5%  |  1500.00 |");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("|  4   | 120  | Lux Soap 150 g      | 34011100 | 30  |  53.00  | 5%  |  1590.00 |");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("|  5   | 125  | Dove soap 125 g     | 34011100 | 25  |  75.00  | 5%  |  1875.00 |");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("|  6   | 129  | Vim bar 200 g       | 08109010 | 20  |  15.00  | 5%  |   300.00 |");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("|  7   | 131  | Pepsodent 150 g     | 33061020 | 30  |  85.00  | 18% |  2550.00 |");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("|                                                    Total:       |    22040.00 |");
+        System.out.println("|                                                    Discounts:   |      140.00 |");
+        System.out.println("|                                                    Grand Total: |    21900.00 |");
+        System.out.println("--------------------------------------------------------------------------------");
+        System.out.println("|                        THANK YOU! VISIT AGAIN                                 |");
+        System.out.println("--------------------------------------------------------------------------------");
+    }
+}
